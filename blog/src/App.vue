7cf22b121e7f2e919285a3e9865dd75d0e1e9ffa
ZD-ong <384474574@qq.com>
2018-05-15 00:39:20 +0800

    add a blog demo

---
+++ blog/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <show-blog></show-blog>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue'
+import showBlog from './components/showBlog.vue'
+
+export default {
+  components: {
+    'addBlog': addBlog,
+    'showBlog': showBlog
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
---
+++ blog/src/components/showBlog.vue
@@ -0,0 +1,82 @@
+<template>
+  <div v-theme:column="'wide'" id="show-blog">
+    <h1>All Blog Articles</h1>
+    <input type="text" v-model="search" placeholder="search blogs...">
+    <div v-for="blog in filteredBlogs" class="single-blog">
+        <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
+        <article>{{blog.body | snippet}}</article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: [],
+      search: ""
+    };
+  },
+  metods: {},
+  created() {
+    this.$http
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(function(data) {
+        console.log(data);
+        this.blogs = data.body.slice(0, 10);
+      });
+  },
+  computed: {
+    filteredBlogs() {
+      return this.blogs.filter(blog => {
+        return blog.title.match(this.search);
+      });
+    }
+  },
+  filters: {
+    toUppercase: function(value) {
+      return value.toUpperCase();
+    },
+    snippet: function(value) {
+      return value.slice(0, 100) + "...";
+    }
+  },
+  directives: {
+    rainbow: {
+      bind(el, binding, vnode) {
+        el.style.color =
+          "#" +
+          Math.random()
+            .toString()
+            .slice(2, 8);
+      }
+    },
+    theme: {
+      bind(el, binding, vnode) {
+        if (binding.value === "wide") {
+          el.style.maxWidth = "1200px";
+        } else if (binding.value === "narrow") {
+          el.style.maxWidth = "560px";
+        }
+        if (binding.arg === "column") {
+          el.style.background = "#ddd";
+          el.style.padding = "20px";
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style>
+#show-blog {
+  max-width: 800px;
+  margin: 0 auto;
+}
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
